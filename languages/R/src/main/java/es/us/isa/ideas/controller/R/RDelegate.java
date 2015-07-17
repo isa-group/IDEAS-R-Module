9f8d6c83dc84bfa9ff0e8f27fd08fd7f30c6f98f
junlun <jlljunior_90@hotmail.com>
2015-07-17 16:16:35 +0200

    Script execution working

--- languages/R/src/main/java/es/us/isa/ideas/controller/R/RDelegate.java
+++ languages/R/src/main/java/es/us/isa/ideas/controller/R/RDelegate.java
@@ -1,15 +1,10 @@
 package es.us.isa.ideas.controller.R;
 
 
-import java.util.List;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.math.R.RserverConf;
 import org.math.R.Rsession;
-import org.rosuda.REngine.REXP;
-import org.rosuda.REngine.REXPMismatchException;
-
-import com.google.common.collect.Lists;
-
 import es.us.isa.ideas.common.AppResponse;
 import es.us.isa.ideas.common.AppResponse.Status;
 
@@ -24,24 +19,28 @@ public class RDelegate {
 	
 	public AppResponse executeScript(String content, String fileUri){
 		AppResponse response= constructBaseResponse(fileUri);
-		String[] commands;
+		
 		try {
 			//Open connection 
 			RserverConf c= RserverConf.parse(host);
-			Rsession s= Rsession.newInstanceTry(System.out, c);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream ps = new PrintStream(baos);
+			Rsession s= Rsession.newInstanceTry(ps, c);
+			s.eval(content);
 			
-			commands= content.split("\n"); 
-			List<String> com= Lists.newArrayList(commands);
+			String font = baos.toString("UTF-8");
+		
+			String font2=font.replace("[eval] "+content, "");
+			String font3= font2.replaceFirst("(org).{1,}", "");
+//			String m= "(!!) Rserve R://localhost:6311 is not accesible.\n! null\n Trying to spawn R://localhost:6311\nEnvironment variables:\nR_HOME=C:\\Program Files\\R\\R-3.2.1\\\nchecking Rserve is available...\nok\nstarting R Daemon... R://localhost:6311\nok\nLocal Rserve started. (Version 103)";
+		
 			
-			List<REXP> exp= Lists.newArrayList();
-			for(String command:com){
-				REXP e=s.eval(command);
-				if(!command.contains("<-"))
-				exp.add(e);
-			}
-			String htmlMessage= asHTML(exp);
-		response.setHtmlMessage(htmlMessage);
+	//		String font4= font3.replaceFirst("(!!) Rserve R://localhost:6311 is not accesible.\n ! null\nTrying to spawn R://localhost:6311\nEnvironment variables:\n R_HOME=C:\\Program Files\\R\\R-3.2.1\\\nchecking Rserve is available...\n ok\nstarting R Daemon... R://localhost:6311\n ok\nLocal Rserve started. (Version 103)", "");
+			String htmlMessage= "<pre>"+font3+"</pre>";
 			
+		
+		response.setHtmlMessage(htmlMessage);			
+			ps.close();
 			s.end();
 			
 		} catch (Exception e) {
@@ -53,7 +52,7 @@ public class RDelegate {
 		return response;
 	}
 	
-	private String asHTML(List<REXP> exp) {
+	/*private String asHTML(List<REXP> exp) {
 		String res="<pre>";		
 		for(REXP tem:exp){
 			try {
@@ -69,13 +68,9 @@ public class RDelegate {
 		}
 		res+="</pre>";
 		return res;
-	}
+	}*/
 
-	private String format(String[] source) {
-		
-		return null;//source.replace("\n", "<br/>");
-	}
-
+	
 	private AppResponse constructBaseResponse(String fileUri) {
 		AppResponse appResponse = new AppResponse();
 		appResponse.setFileUri(fileUri);
@@ -83,4 +78,32 @@ public class RDelegate {
 		return appResponse;
 	}
 
+	/*private Boolean evalCommand(String command, Rsession s2,REXP result) {
+		Boolean res=false;
+		if(checkNotprintingCommand(command)){
+			result=null;
+			res=s2.voidEval(command);
+			
+		}else{
+			result= s2.eval(command);
+			
+			res=true;
+		}
+		
+		return res;
+	}*/
+
+	/*private Boolean checkNotprintingCommand(String command) {
+		Boolean res= false;
+		List<String> fc= Lists.newArrayList();
+		fc.add("while");fc.add("for");fc.add("if");fc.add("repeat");fc.add("break");
+		fc.add("next");fc.add("{");fc.add("}");fc.add("<-");fc.add("#");
+		for(String t:fc){
+			if(command.contains(t))
+			  res=true;	
+				break;
+		}
+		
+		return res;
+	}*/
 }
--- languages/R/src/test/java/ConnectionTest.java
+++ languages/R/src/test/java/ConnectionTest.java
@@ -1,5 +1,7 @@
 import org.rosuda.REngine.REXP;
 
+import com.google.common.collect.Lists;
+
 import org.junit.Test;
 import org.math.R.RserverConf;
 import org.math.R.Rsession;
@@ -11,11 +13,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class ConnectionTest {
 	Rsession s;
 	String local6311="R://localhost:6311";
 	String script= "src/test/resources/Script_Prueba_1.R";
+	String scriptContent= "x <- c(1,2,4,8,16)\nfor (loop in x)\n{\n  cat(\"value of loop: \",loop,\"\n\");\n}";
+	String script2= "src/test/resources/Script_Prueba_2.R";
 	@Test
 	public void testConnection0(){
 		
@@ -84,4 +89,115 @@ public class ConnectionTest {
 		 s.end();
 	}
 	
+	//@Test	
+	public void testConnection3(){
+		Properties p= new Properties();
+		p.put("r-source", "enable");
+		RserverConf c= RserverConf.parse(local6311);
+		c.properties=p;
+		Rsession s= Rsession.newInstanceTry(System.out, c);
+		 
+		 File archivo = new File (script2);
+		 FileReader fr;
+		 BufferedReader br;
+		 List<String> output= new ArrayList<String>();
+		 String spec="value of loop: 1\nvalue of loop: 2\nvalue of loop: 4\nvalue of loop: 8\nvalue of loop: 16";
+		try {
+			fr = new FileReader (archivo);
+			br  = new BufferedReader(fr);
+			String linea;
+			 while((linea=br.readLine())!=null){
+				 //linea=br.readLine();
+				 if(linea!=null&&linea!=""){
+					 String asS= s.eval(linea).asString();
+					 if(asS!=null&&asS!="")
+						 output.add(asS);
+			}
+			 }			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		assertNotNull(output);
+		assertEquals( spec,output.get(output.size()-1));
+		 
+		 s.end();
+	}
+	
+	//@Test
+	public void testConnection4(){
+		String command1="x <- c(1,2,4,8,16)";
+		String command= "for (loop in x){ cat(\"value of loop: \",loop,\"\n\");}";
+		String spec="value of loop: 1\nvalue of loop: 2\nvalue of loop: 4\nvalue of loop: 8\nvalue of loop: 16";
+		String result="";
+		try {
+			RserverConf c= RserverConf.parse(local6311);
+			 s= Rsession.newInstanceTry(System.out, c);
+			loop(command1,s);
+			result= loop(command,s);
+			
+			s.end();
+		} catch (Exception o) {
+			
+			o.printStackTrace();
+		}
+		
+		assertEquals(spec, result);
+	}
+
+	//@Test
+	public void testConnection5(){
+		Properties p= new Properties();
+		p.put("r-source", "r-source enable");
+		/*RserverConf c= RserverConf.parse(local6311);
+		c.properties=p;*/
+		RserverConf c= new RserverConf("localhost", 6311, null, null, p);
+		Rsession s= Rsession.newInstanceTry(System.out, c);
+		String command= "for (loop in x){ cat(\"value of loop: \",loop,\"\n\");}";
+		REXP e= s.eval(command);
+		
+		
+		s.end();
+	}
+	
+	//@Test
+	public void testConnection6(){
+		
+		try {
+			RserverConf c= RserverConf.parse(local6311);
+			 s= Rsession.newInstanceTry(System.out, c);
+			 	String res= s.asString("eval(parse("+scriptContent+"))");
+			s.end();
+		} catch (Exception o) {
+			
+			o.printStackTrace();
+		}
+		
+	}
+	
+	private String loop(String command, Rsession s2) {
+		String res="";
+		
+		if(checkNotPrintingCommand(command)){
+			s2.voidEval(command);
+		}else{
+			res= s2.asString(command);
+		}
+		
+		return res;
+	}
+
+	private Boolean checkNotPrintingCommand(String command) {
+		Boolean res= false;
+		List<String> fc= Lists.newArrayList();
+		fc.add("while");fc.add("for");fc.add("if");fc.add("repeat");fc.add("break");
+		fc.add("next");fc.add("{");fc.add("}");fc.add("<-");
+		for(String t:fc){
+			if(command.contains(t))
+			  res=true;	
+				break;
+		}
+		
+		return res;
+	}
 }
